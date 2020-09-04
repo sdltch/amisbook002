b697532b59eeb5177aeec6e773b2bf2f8909e597
毛豆 <596211206@qq.com>
2020-09-04 14:54:17 +0800

    测试

--- start.sh
+++ start.sh
@@ -6,7 +6,11 @@
         echo "启动测试脚本"
         mvn clean compile install package
         dates=`date "+%Y-%m-%d_%H:%M:%S"`linuxreport.html
-        mv amisrobot/linuxreport/linuxreport.html amisrobot/linuxreport/reporthistory/$dates
+        myFile="/root/.jenkins/workspace/amisbook002/amisrobot/linuxreport/linuxreport.html"
+        # 这里的-f参数判断$myFile是否存在
+        if [ -f "$myFile" ]; then
+          mv amisrobot/linuxreport/linuxreport.html amisrobot/linuxreport/reporthistory/$dates
+        fi
         mvn exec:java -Dexec.mainClass="com.example.amisbook002.MainStart"
 
 
